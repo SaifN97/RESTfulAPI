ff5a3e8d170ebb040dc11b0d6e0516fac554b234
SaifN97 <saifnarpali@gmail.com>
2020-12-07 19:08:13 +0530

    Model relationships and routes setup

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,20 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Buyers
+Route::resource('buyers', 'Buyers\BuyerController', ['only' => ['index', 'show']]);
+
+//Category
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+
+//Seller
+Route::resource('seller', 'Seller\SellerBuyerController', ['only' => ['index', 'show']]);
+
+//Products
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+
+//Transactions
+Route::resource('Transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+//Users
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
